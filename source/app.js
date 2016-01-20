f19c06c225930007344611b6ddd031cce4dd5458
Teng-hao Chang <zhusee2@gmail.com>
2016-01-20 17:55:45 +0800

    Summary board

--- source/app.js
+++ source/app.js
@@ -1,13 +1,22 @@
 import notificationTemplate from './templates/notification'
+import summaryBoardTemplate from './templates/summary_board'
 import './css/app.scss'
 
 const icon_url = 'http://images.apple.com/v/mac/shared/osx/f/images/builtinapps/safari_nav_large.png';
 const rootNode = document.querySelector('#summary-view-root');
+
+const htmlToNode = (htmlString) => {
+  let containerNode = document.createElement('div');
+  containerNode.innerHTML = htmlString;
+  return containerNode.children[0];
+}
 
 let html = notificationTemplate({
   title: 'Frequent visitor',
   desc: 'Continuous log in for 30 days.',
   icon_url
 });
+rootNode.appendChild( htmlToNode(html) );
 
-rootNode.innerHTML = html;
+html = summaryBoardTemplate({});
+rootNode.appendChild( htmlToNode(html) );
