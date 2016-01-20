1db1f101cebc6d9f2eca1e08287fdb545bbe11a5
Teng-hao Chang <zhusee2@gmail.com>
2016-01-20 15:04:40 +0800

    Appearance for notification widget

--- source/app.js
+++ source/app.js
@@ -1,3 +1,13 @@
 import notificationTemplate from './templates/notification'
+import './css/app.scss'
 
-console.log(notificationTemplate);
+const icon_url = 'http://images.apple.com/v/mac/shared/osx/f/images/builtinapps/safari_nav_large.png';
+const rootNode = document.querySelector('#summary-view-root');
+
+let html = notificationTemplate({
+  title: 'Frequent visitor',
+  desc: 'Continuous log in for 30 days.',
+  icon_url
+});
+
+rootNode.innerHTML = html;
