fe74026c51f1b84cc49507eec81edc80f50a0191
Teng-hao Chang <zhusee2@gmail.com>
2016-01-21 15:39:49 +0800

    Implement API integration interface

--- source/app.js
+++ source/app.js
@@ -1,22 +1,75 @@
-import notificationTemplate from './templates/notification'
-import summaryBoardTemplate from './templates/summary_board'
-import './css/app.scss'
+import NotificationStack from './notification_stack'
 
-const icon_url = 'http://images.apple.com/v/mac/shared/osx/f/images/builtinapps/safari_nav_large.png';
-const rootNode = document.querySelector('#summary-view-root');
+const API_PREFIX = 'https://floating-garden-1441.herokuapp.com/api/v1';
 
-const htmlToNode = (htmlString) => {
-  let containerNode = document.createElement('div');
-  containerNode.innerHTML = htmlString;
-  return containerNode.children[0];
+class GaAs {
+  constructor() {
+    throw Error('[GaAs] Please use GaAs.init() instead of newing an instance.')
+    return false;
+  }
+
+  static init(options={}) {
+    if (this._initialized) {
+      throw Error('[GaAs] GaAs has already been initialized.');
+    }
+    if (!options.user_id) {
+      throw Error('[GaAs] Unable to init without user_id.')
+    }
+    if (!options.api_token) {
+      throw Error('[GaAs] Unable to init without api_token.')
+    }
+
+    this._user_id = options.user_id;
+    this._api_token = options.api_token;
+    this._notificationStatck = new NotificationStack;
+    this._initialized = true;
+  }
+
+  static track(event_name) {
+    // 1. POST /events
+    // 2. Check achivements in response
+    // 3. Append notification to body
+
+    let queryParams = this._getQueryParams();
+    queryParams = queryParams + `&name=${ encodeURIComponent(event_name) }`;
+
+    fetch(API_PREFIX + '/events', {
+      method: 'POST',
+      body: queryParams
+    }).then( response => response.json() )
+      .then( response => {
+        console.log(response.achivements);
+      });
+
+  }
+
+  static renderSummaryBoardTo(rootNode) {
+    // 1. GET /achievements
+    // 2. Render summary board
+    // 3. Append to rootNode
+
+    let queryParams = this._getQueryParams();
+
+    fetch(API_PREFIX + `/achievements?${ queryParams }`)
+      .then( response => response.json() )
+      .then( achivementsArray => {
+        for (let i in achivementsArray) {
+          console.log(achivementsArray[i]);
+        }
+      });
+  }
+
+  static _getQueryParams() {
+    if ( !(this._user_id && this._api_token) ) {
+      throw Error('[GaAs] Please init with proper data before use.');
+      return null;
+    }
+
+    let params = `user_id=${ encodeURIComponent(this._user_id) }`;
+    params = params + `&api_token=${ encodeURIComponent(this._api_token) }`;
+
+    return params
+  }
 }
 
-let html = notificationTemplate({
-  title: 'Frequent visitor',
-  desc: 'Continuous log in for 30 days.',
-  icon_url
-});
-rootNode.appendChild( htmlToNode(html) );
-
-html = summaryBoardTemplate({});
-rootNode.appendChild( htmlToNode(html) );
+module.exports = GaAs
